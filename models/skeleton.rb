ad31f4e3a2323c5c7b97089a45a3795836337dc6
Simon <simon.neutert@gmail.com>
2018-06-30 11:21:04 +0200

    validates presence

--- models/skeleton.rb
+++ models/skeleton.rb
@@ -1,5 +1,6 @@
 # sample Model model with a validation
 # hook is set in the corresponding route for saving a model
 class Skeleton < ActiveRecord::Base
+  validates :name, presence: true
   validates :name, uniqueness: true
 end
